120324482d805f82184d9fe2ffb6573d81692762
matiasdn <matiasdn@metropolia.fi>
2023-03-18 19:44:50 +0200

    8.1-8.7

--- osa8/8.1-8.7/index.js
+++ osa8/8.1-8.7/index.js
@@ -124,6 +124,10 @@ const typeDefs = `
         published: Int!
         genres: [String!]!
     ): Book
+    editAuthor(
+        name: String!
+        setBornTo: Int!
+    ): Author
   }
 `;
 
@@ -159,6 +163,15 @@ const resolvers = {
         authors = authors.concat(author);
       }
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) return null;
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
