35a890a30128b9b2a50b3991260d9931752164f9
matiasdn <matiasdn@metropolia.fi>
2023-03-18 19:18:07 +0200

    8.1-8.6

--- osa8/8.1-8.7/index.js
+++ osa8/8.1-8.7/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -112,9 +113,18 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   },
+  
+  type Mutation {
+    addBook(
+        title: String!
+        author: String!
+        published: Int!
+        genres: [String!]!
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -122,16 +132,33 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books;
-      }
-      return books.filter((book) => book.author === args.author);
+      if (!args) return books;
+      const { author, genre } = args;
+      let booksToReturn = books;
+      if (author)
+        booksToReturn = booksToReturn.filter((book) => book.author === author);
+      if (genre)
+        booksToReturn = booksToReturn.filter((book) =>
+          book.genres.includes(genre)
+        );
+      return booksToReturn;
     },
     allAuthors: () => authors,
   },
   Author: {
     bookCount: (root) => {
       return books.filter((book) => book.author === root.name).length;
+    },
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      if (!authors.find((author) => author.name === args.author)) {
+        const author = { name: args.author, id: uuid() };
+        authors = authors.concat(author);
+      }
+      return book;
     },
   },
 };
